cb621fa0307e35cddfdf83341b1f1e54cfb11226
Miguel Hernandez Gamarra <67189138+mighg@users.noreply.github.com>
2024-03-26 16:34:47 +0100

    Update 8_Cell_line_selector.py

--- pages/8_Cell_line_selector.py
+++ pages/8_Cell_line_selector.py
@@ -204,7 +204,7 @@ if st.button('Find cell lines', type='primary'):
             )
             st.dataframe(table_data, hide_index=True)  
             csv = table_data.to_csv(encoding='utf-8', index=False)
-            b64 = base64.64encode(csv.encode()).decode()
+            b64 = base64.b64encode(csv.encode()).decode()
             href = f'<a href="data:file/csv;base64,{b64}" download="csv.csv">Download CSV File</a>'
             st.markdown(href, unsafe_allow_html=True)
 
