0b2cc99edfa16bc62a115db3be3ae0c0990df118
Miguel Hernandez Gamarra <67189138+mighg@users.noreply.github.com>
2024-05-17 19:54:01 +0200

    Update 2_Tumor_expression_change.py

--- pages/2_Tumor_expression_change.py
+++ pages/2_Tumor_expression_change.py
@@ -123,8 +123,8 @@ else:
 HPA_membrane = ""
 for gene in correct_genes:
     if gene in experimental_pm_genes:
-        HPA_membrane.replace('and',',')
-        HPA_membrane += f'{gene} and '
+        HPA_membrane += f'{gene} , '
+HPa_membrane = HPA_membrane.replace(',','and')
 if HPA_membrane[-4:] == 'and ':
     HPA_membrane = HPA_membrane[:-5]
 st.write(HPA_membrane)
