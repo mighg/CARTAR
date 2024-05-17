0a7b66ae0f44e108c5a2544fbb587eb283d9b225
Miguel Hernandez Gamarra <67189138+mighg@users.noreply.github.com>
2024-05-17 19:43:06 +0200

    Update 2_Tumor_expression_change.py

--- pages/2_Tumor_expression_change.py
+++ pages/2_Tumor_expression_change.py
@@ -125,6 +125,8 @@ for gene in correct_genes:
     if gene in experimental_pm_genes:
         HPA_membrane.replace('and',',')
         HPA_membrane += f'{gene} and '
+if HPA_membrane[-5:] == 'and ':
+    HPA_membrane = HPA_membrane[-5:]
 tumors = st.multiselect('Select tumors (optional)', tumor_options)
 # Expander to show abbreviation meaning
 with st.expander('Extension of tumor abbreviations\' meaning'):
